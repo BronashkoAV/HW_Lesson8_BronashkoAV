43552432f4aa4c103c3c71b34adca82d36d7f85e
BronashkoAV <BronashkoAV@gmail.com>
2018-08-16 22:05:01 +0300

    Add Form

--- HW_Lesson8_BronashkoAV/FormBeliveOrNotBelive.cs
+++ HW_Lesson8_BronashkoAV/FormBeliveOrNotBelive.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace HW_Lesson8_BronashkoAV
 {
-    public partial class Form1 : Form
+    public partial class FormBeliveOrNotBelive : Form
     {
-        public Form1()
+        public FormBeliveOrNotBelive()
         {
             InitializeComponent();
         }
--- HW_Lesson8_BronashkoAV/Program.cs
+++ HW_Lesson8_BronashkoAV/Program.cs
@@ -16,7 +16,7 @@ namespace HW_Lesson8_BronashkoAV
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormBeliveOrNotBelive());
         }
     }
 }
